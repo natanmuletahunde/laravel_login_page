ff11814d184d6c04d4e2e1a53521ee1f35996f14
pivnati <natanmuleta911@gmail.com>
2024-09-01 19:38:44 -0700

    add the facades inside the welcome page

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
-use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LoginController;
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'storeLoginData']);
