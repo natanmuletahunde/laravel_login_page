b83ffec6a71b2c28760258035fb1ae45acb3693c
pivnati <natanmuleta911@gmail.com>
2024-09-01 19:22:39 -0700

    modify the  logincontroller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class LoginController extends Controller
 {
-    //
+    public function showLoginForm()
+    {
+        return view('login');
+    }
+
+    public function storeLoginData(Request $request)
+    {
+        // Validate incoming request
+        $validatedData = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        // Store login data into the database
+        User::create([
+            'email' => $validatedData['email'],
+            'password' => bcrypt($validatedData['password']),
+        ]);
+
+        return redirect()->back()->with('success', 'Login data stored successfully!');
+    }
 }
